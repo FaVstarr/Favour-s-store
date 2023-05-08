34de024e8ef49880ea6f66a30ec76bc0244b9644
FavBoi <favourchamberlain32@gmail.com>
2023-05-08 16:23:54 +0100

    removed useState import

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,5 +1,5 @@
 import { ShoppingBagIcon } from "@heroicons/react/24/outline";
-import React, { useState } from 'react';
+import React from 'react';
 import '../App.css'
 import { useNavigate } from "react-router-dom";
 
